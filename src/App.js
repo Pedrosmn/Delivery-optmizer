a00ccd75ebe888fdb3447575a568ab179d672fc6
unknown <opedrosantos16@gmail.com>
2025-06-12 18:32:37 -0300

    domains

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import { Deposito } from './Deposito.js';
 import { Hub } from './Hub.js';
 import { Rota } from './Rota.js';
 import { Grafo } from './Grafo.js';
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
